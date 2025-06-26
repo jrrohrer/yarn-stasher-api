43b3f35e1c7c7715c8b1758a410b636b5266dae8
Jessie Rohrer <35381492+jrrohrer@users.noreply.github.com>
2025-06-25 22:57:47 -0400

    configure permitted params for user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :email, :password ])
+  end
+
+  # Add any additional methods or filters here
 end
